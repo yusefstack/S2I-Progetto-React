9d35626193abf1c0e73a586b45530c41264c7953
yusefstack <giustack02@gmail.com>
2023-05-09 17:34:43 +0200

    Inizializzazione progetto

---
+++ functions/mealsInfo.js
@@ -0,0 +1,26 @@
+const axios = require('axios')
+
+exports.handler = async function(event, context) {
+
+    const {query} = event.queryStringParameters
+    
+    const options = {
+        method: "GET",
+        url: `https://api.spoonacular.com/recipes/complexSearch?apiKey=${process.env.API_KEY}&query=${query}`,
+        headers: {
+        "Content-Type": "application/json",
+        },
+    }
+
+    return {
+        statusCode: 200,
+        body: JSON.stringify(   
+        await axios.request(options)
+            .then(response => {
+            return response.data 
+            })
+            .catch(err => {
+            return err
+            }))
+    }
+}
